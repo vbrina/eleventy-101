bdbe00e08b68621a48879a51c27f6b0868c3e6bc
Vitor Petruz Brina <vitor.brina@venturus.org.br>
2022-03-24 17:42:23 -0300

    Added some styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addWatchTarget("src/css/");
 
   return {
     dir: {
